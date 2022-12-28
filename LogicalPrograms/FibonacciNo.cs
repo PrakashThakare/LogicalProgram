06ae57c66c6b7898580310ae90fb0e24c0929fa0
Shree Thakre <shreet365@gmail.com>
2022-12-28 15:50:06 +0530

    Initial commit

---
+++ LogicalPrograms/FibonacciNo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciNo
+    {
+       public static void FibonacciNO(int iValue)
+        {
+            int iValue2 = 1, iSum = 0,iValue3=0;
+            for (int iCnt=0 ; iCnt<iValue ; iCnt++)
+            {
+                iSum =iValue3+iValue2; 
+                iValue3 = iValue2; 
+                iValue2 = iSum;
+                Console.WriteLine(iSum);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Number For Fibonacci Number");
+            int iNo = Convert.ToInt32(Console.ReadLine());
+            FibonacciNo.FibonacciNO(iNo);
+            Console.ReadLine();
+        }
+    }
+}
